aae2c12a0f15001baf3bf9577e38c39e2a7f4b31
rahataliattari <rahataliattari@gmail.com>
2026-08-10 18:54:29 +0500

    Add files via upload

---
+++ config.js
@@ -0,0 +1,5 @@
+// Future live-data configuration.
+// Put your Google Apps Script Web App URL here after deploying the API.
+// Example:
+// const API_CONFIG = { url: "https://script.google.com/macros/s/YOUR_DEPLOYMENT_ID/exec" };
+const API_CONFIG = { url: "" };
